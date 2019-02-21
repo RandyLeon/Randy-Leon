39cceda07326018367afcb87a96cb702583904ba
Randy Leon <randy.leon123@outlook.com>
2019-02-21 00:43:08 -0500

    Save changes

--- quadratic/Program.cs
+++ quadratic/Program.cs
@@ -10,6 +10,25 @@ namespace quadratic
     {
         static void Main(string[] args)
         {
+            double a, b, c, x1, x2;
+            Console.WriteLine("Enter a");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter b");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter c");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            double Root = Math.Sqrt(b * b - 4 * a * c);
+            x1 = (-b - Root) / 2 * a;
+            x2 = (-b + Root) / 2 * a;
+
+            Console.WriteLine("x1 = " + x1);
+            Console.WriteLine("x2 = " + x2);
+            Console.ReadLine();
+
+                   
+                    
+            
         }
     }
 }
